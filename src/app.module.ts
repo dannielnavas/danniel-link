abf87b51e3343290e25381af5da38c45f2f23d5b
Danniel Navas <dannielnavas@gmail.com>
2024-09-18 21:29:35 -0500

    ✨ Agregar soporte para Mongoose en el módulo de la aplicación y definir el esquema de ShortUrl en package.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import config from './config';
 import { DatabaseModule } from './database/database.module';
+import { ShortUrl, ShortUrlSchema } from './entities/app.entity';
 import { environments } from './environments';
 
 @Module({
@@ -14,6 +16,12 @@ import { environments } from './environments';
       isGlobal: true,
       load: [config],
     }),
+    MongooseModule.forFeature([
+      {
+        name: ShortUrl.name,
+        schema: ShortUrlSchema,
+      },
+    ]),
   ],
   controllers: [AppController],
   providers: [AppService],
