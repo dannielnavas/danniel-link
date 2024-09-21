9d19072168c9071a2659b0f700500b78ea3f0d15
Danniel Navas <dannielnavas@gmail.com>
2024-09-20 20:20:10 -0500

    ✨ Cambiar excepción NotFoundException por ForbiddenException en la validación de claves

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateShotLinkDto } from './dtos/app.dto';
@@ -16,11 +20,8 @@ export class AppService {
   }
 
   async create(payload: CreateShotLinkDto) {
-    if (!payload.key) {
-      throw new NotFoundException('Key is required');
-    }
     if (payload.key !== process.env.KEY) {
-      throw new NotFoundException('Invalid key');
+      throw new ForbiddenException('Invalid key');
     }
     const link = await this.shortUrlModel.findOne({
       identifier: payload.identifier,
