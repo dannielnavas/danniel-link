d31fefa9d5530f48c1a8cb14ade5fac1763775d1
Danniel Navas <dannielnavas@gmail.com>
2024-10-24 18:09:33 -0500

    ✨ Configurar Vercel para incluir archivos de vistas; actualizar ESLint a la versión 9.13.0; corregir sintaxis en la plantilla 404.pug y configurar el uso de Pug en main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  // Configurar la carpeta pública para archivos estáticos
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  // Configurar las vistas y el motor de plantillas Pug
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('pug');
 
